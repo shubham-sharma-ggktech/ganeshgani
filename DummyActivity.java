e1032fe13dbc10bf530ee59acf700eea2642bff1
shubham-sharma-ggktech <ssharma0834@gmail.com>
2017-07-07 11:40:59 +0000

    debugged
    champagne is not ccompatable with all mobile devices

--- DummyActivity.java
+++ DummyActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Typeface tf1=Typeface.createFromAsset(getAssets(),"champagne.ttf");
+        Typeface tf1=Typeface.createFromAsset(getAssets(),"membra.ttf");
         TextView subtitle1= (TextView) findViewById(R.id.subtitle1);
         Typeface tf2=Typeface.createFromAsset(getAssets(),"membra.ttf");
         TextView heading1=(TextView) findViewById(R.id.heading1);
